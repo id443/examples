97668e5298dce1580c46ac4bd660acfe9d6d2bcc
id443 <mksmsmirnov@gmail.com>
2024-07-20 10:24:49 +0300

    Добавил заместителя

---
+++ javaExamples/заместитель/IMath.java
@@ -0,0 +1,12 @@
+package заместитель;
+
+public interface IMath {
+
+	public double add(double x, double y);
+
+	public double sub(double x, double y);
+
+	public double mul(double x, double y);
+
+	public double div(double x, double y);
+}
---
+++ javaExamples/заместитель/Math.java
@@ -0,0 +1,20 @@
+package заместитель;
+
+public class Math implements IMath {
+
+        public double add(double x, double y) {
+            return x + y;
+        }
+    
+        public double sub(double x, double y) {
+            return x - y;
+        }
+    
+        public double mul(double x, double y) {
+            return x * y;
+        }
+    
+        public double div(double x, double y) {
+            return x / y;
+        }
+    }
