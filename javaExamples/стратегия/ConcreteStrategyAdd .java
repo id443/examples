b20b482ff50e1bfb1a954cb31d387b3c61d93e04
id443 <mksmsmirnov@gmail.com>
2024-07-21 10:07:49 +0300

    Стратегия

---
+++ javaExamples/стратегия/ConcreteStrategyAdd .java
@@ -0,0 +1,9 @@
+package стратегия;
+
+class ConcreteStrategyAdd implements Strategy {
+ 
+    public int execute(int a, int b) {
+        System.out.println("Called ConcreteStrategyAdd's execute()");
+        return a + b;  // Do an addition with a and b
+    }
+}
---
+++ javaExamples/стратегия/ConcreteStrategyMultiply.java
@@ -0,0 +1,9 @@
+package стратегия;
+
+class ConcreteStrategyMultiply implements Strategy {
+ 
+    public int execute(int a, int b) {
+        System.out.println("Called ConcreteStrategyMultiply's execute()");
+        return a * b;   // Do a multiplication with a and b
+    }    
+}
