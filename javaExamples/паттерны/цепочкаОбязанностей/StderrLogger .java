3a7699ed9d386611853c4e7837f53c016058d596
id443 <mksmsmirnov@gmail.com>
2024-07-24 07:54:52 +0300

    цепочка обязанностей

---
+++ javaExamples/паттерны/цепочкаОбязанностей/StderrLogger .java
@@ -0,0 +1,11 @@
+package паттерны.цепочкаОбязанностей;
+
+class StderrLogger extends Logger {
+    public StderrLogger(int mask) {
+        this.mask = mask;
+    }
+
+    protected void writeMessage(String msg) {
+        System.out.println("Sending to stderr: " + msg);
+    }
+}
