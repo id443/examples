48855fdf1b7677748d72f94fbe6ff78645fa629b
Maxim <32535933+id443@users.noreply.github.com>
2024-08-07 10:52:43 +0300

    Create CPU.java

---
+++ javaExamples/паттерны/фасад/CPU.java
@@ -0,0 +1,13 @@
+class CPU {
+    public void freeze() {
+        System.out.println("freeze");
+    }
+
+    public void jump(long position) {
+        System.out.println("jump position = " + position);
+    }
+
+    public void execute() {
+        System.out.println("execute");
+    }
+}
