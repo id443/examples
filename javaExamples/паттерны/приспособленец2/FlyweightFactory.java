fffc534395e4ee0acbd38655bd6898278d660795
Maxim <32535933+id443@users.noreply.github.com>
2024-08-09 09:50:46 +0300

    Create FlyweightFactory.java

---
+++ javaExamples/паттерны/приспособленец2/FlyweightFactory.java
@@ -0,0 +1,27 @@
+public class FlyweightFactory {
+
+	private HashMap<Integer, EnglishCharacter> characters = new HashMap();
+	
+	public EnglishCharacter getCharacter(int characterCode){
+		EnglishCharacter character = characters.get(characterCode);
+		if (character == null){
+			switch (characterCode){
+				case 1 : {
+					character = new CharacterA();
+					break;
+				}
+				case 2 : {
+					character = new CharacterB();
+					break;
+				}
+				case 3 : {
+					character = new CharacterC();
+					break;
+				}
+			}
+			characters.put(characterCode, character);
+		}
+		return character;
+	} 
+	
+}
