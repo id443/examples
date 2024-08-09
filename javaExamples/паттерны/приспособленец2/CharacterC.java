e3d01af501041bc189e381cec4b49a042b5f1a9b
Maxim <32535933+id443@users.noreply.github.com>
2024-08-09 09:49:13 +0300

    Create CharacterC.java

---
+++ javaExamples/паттерны/приспособленец2/CharacterC.java
@@ -0,0 +1,14 @@
+public class CharacterC extends EnglishCharacter {
+
+	public CharacterC(){
+		symbol = 'C';
+		width = 40;
+		height = 50;
+	}
+	
+	@Override
+	public void printCharacter() {
+		System.out.println("Symbol = " + symbol + " Width = " + width + " Height = " + height);
+	}
+	
+}
