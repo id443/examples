f6ef1233cde88c459d626de0c30a40b583cf457e
id443 <mksmsmirnov@gmail.com>
2024-08-10 08:15:50 +0300

    прокси

---
+++ javaExamples/паттерны/прокси/Main.java
@@ -0,0 +1,17 @@
+package паттерны.прокси;
+
+import паттерны.заместитель.IMath;
+import паттерны.заместитель.MathProxy;
+
+public class Main {
+    public static void main(String[] args) {
+		// Create math proxy
+		IMath p = new MathProxy();
+
+		// Do the math
+		System.out.println("4 + 2 = " + p.add(4, 2));
+		System.out.println("4 - 2 = " + p.sub(4, 2));
+		System.out.println("4 * 2 = " + p.mul(4, 2));
+		System.out.println("4 / 2 = " + p.div(4, 2));
+	}
+}
