96dc0acdc449b4f48c25c60a34e0130d6b9bfda0
Maxim <32535933+id443@users.noreply.github.com>
2024-08-21 12:03:26 +0300

    Update AtomicVolitileSynchronyzed.java

--- javaExamples/multithreading/AtomicVolitileSynchronyzed.java
+++ javaExamples/multithreading/AtomicVolitileSynchronyzed.java
@@ -1,3 +1,11 @@
 public class AtomicVolitileSynchronyzed {
+    private final AtomicInteger counter = new AtomicInteger(0);
     
+    int getValue() {
+        return counter.get();
+    }
+    
+    void increment() {
+        counter.incrementAndGet();
+    }
 }
