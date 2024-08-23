98f0c111b57e949dfcb150d3b834318b6789272a
id443 <mksmsmirnov@gmail.com>
2024-08-23 18:20:28 +0300

    Перестроил

---
+++ javascript/arrowFunctions.js
@@ -0,0 +1,10 @@
+const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
+
+console.log(materials.map((material) => material.length));
+
+function bob(a) {
+    return a + 100;
+  }
+  
+  // Arrow Function
+  const bob2 = (a) => a + 100;
